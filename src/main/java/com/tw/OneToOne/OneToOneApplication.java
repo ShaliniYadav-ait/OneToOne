a37f0d50b7bbcdfcf204b45118341ffdf1d1fcad
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-09-03 22:13:43 +0530

    Initial setup

---
+++ src/main/java/com/tw/OneToOne/OneToOneApplication.java
@@ -0,0 +1,15 @@
+package com.tw.OneToOne;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class OneToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToOneApplication.class, args);
+	}
+
+}
