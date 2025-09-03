cc4daa45f14b0ceecc476d24da13f12e1dd5d068
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-09-03 22:48:02 +0530

    Annotate impl class with repository

--- src/main/java/com/tw/OneToOne/dao/AppDAOImpl.java
+++ src/main/java/com/tw/OneToOne/dao/AppDAOImpl.java
@@ -3,9 +3,10 @@ package com.tw.OneToOne.dao;
 import com.tw.OneToOne.entity.Instructor;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-
+@Repository
 public class AppDAOImpl implements AppDAO {
 
     private final EntityManager theEntityManager;
