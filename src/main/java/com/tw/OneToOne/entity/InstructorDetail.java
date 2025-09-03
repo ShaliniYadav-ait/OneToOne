9af6c1d70bd46ccfa47aa68da4f9f4eb7ead80da
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-09-03 22:25:52 +0530

    add entity and relevant details

---
+++ src/main/java/com/tw/OneToOne/entity/InstructorDetail.java
@@ -0,0 +1,62 @@
+package com.tw.OneToOne.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "instructor")
+public class InstructorDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    public InstructorDetail() {
+
+    }
+
+    public InstructorDetail(String id, String youtubeChannel, String hobby) {
+        this.id = id;
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    @Override
+    public String toString() {
+        return "InstructorDetail{" +
+                "id='" + id + '\'' +
+                ", youtubeChannel='" + youtubeChannel + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}
