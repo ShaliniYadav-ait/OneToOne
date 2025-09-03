5cb52150156b02983f18eeec2f1a736d92765e45
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-09-03 22:56:39 +0530

    add find by Id for instructor in main app

--- src/main/java/com/tw/OneToOne/OneToOneApplication.java
+++ src/main/java/com/tw/OneToOne/OneToOneApplication.java
@@ -17,8 +17,16 @@ public class OneToOneApplication {
     public CommandLineRunner commandLineRunner(AppDAO appDAO) {
         return runner -> {
 //            createInstructor(appDAO);
+//            findInstructor(appDAO);
         };
     }
+
+//    private void findInstructor(AppDAO appDAO) {
+//        int theId = 1;
+//        System.out.println("Finding id : " + theId);
+//        Instructor tempInstructor = appDAO.findInstructorById(theId);
+//        System.out.println("Instructor is : " + tempInstructor);
+//    }
 //
 //    private void createInstructor(AppDAO appDAO) {
 //        Instructor tempInstructor = new Instructor("Shalini", "Yadav", "shal123434@gmail.com");
